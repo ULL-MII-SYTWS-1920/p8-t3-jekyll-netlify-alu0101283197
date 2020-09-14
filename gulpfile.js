91e0a701f2906607033fcfba96d7cf31cb6077af
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-09-14 13:01:22 +0000

    Deploy ULL-MII-SYTWS-1920/p8-t3-jekyll-netlify-Felipe5Valencia to github.com/ULL-MII-SYTWS-1920/p8-t3-jekyll-netlify-Felipe5Valencia.git:gh-pages

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const run = require('gulp-run'); 
+const browserSync = require('browser-sync').create();
+
+const siteRoot = '_site';
+const shellCommand = 'bundle exec jekyll build --config _config.yml';
+
+
+gulp.task('sass', () => {
+    return gulp.src('./_sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./_site/css'));
+});
+
+gulp.task('build:jekyll',()=> {    
+    return gulp.src('.', {allowEmpty: true})
+      .pipe(run(shellCommand))
+      .on('error', console.log);
+  });
+
+gulp.task('serve', () => {
+  browserSync.init({
+    files: [siteRoot + '/**'],
+    port: 4000,
+    server: {
+      baseDir: siteRoot
+    }
+  });
+});
+
+gulp.task('default', gulp.series(['sass']));
+
